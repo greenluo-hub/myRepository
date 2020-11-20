dcc2f6fdf6976fd1b1f0eec1a9c3246dead0b431
greenluo-hub <1242040426@qq.com>
2020-11-20 09:24:43 +0800

    新项目

---
+++ demo23/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace demo23
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(185, 80);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 28);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(290, 80);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 28);
+            this.btnStop.TabIndex = 0;
+            this.btnStop.Text = "结束";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 404);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(776, 23);
+            this.progressBar.TabIndex = 1;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(513, 80);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "计算";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnCalc;
+    }
+}
+
---
+++ demo23/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace demo23
+{
+    public partial class Form1 : Form
+    {
+        private volatile bool CancelWork = false;
+        private ManualResetEvent manualReset = new ManualResetEvent(false);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            btnStart.Enabled = false;
+            manualReset.Reset();
+            Task.Run(() =>
+            {
+                RunWork();
+            });
+        }
+
+        private void RunWork()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                this.Invoke(new Action(() =>
+                {
+                    this.progressBar.Value = i;
+                }));
+
+                Thread.Sleep(1000);
+
+                if (manualReset.WaitOne(1000))
+                {
+                    break;
+                }
+            }
+
+            this.Invoke(new Action(() =>
+            {
+                this.btnStart.Enabled = true;
+                this.btnStop.Enabled = false;
+            }));
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            manualReset.Reset();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            Parallel.For(1, 100, x =>
+            {
+                bool b = IsPrimeNumber(x);
+                Console.WriteLine($"{x}:{b}");
+            });
+        }
+
+        private static bool IsPrimeNumber(int number)
+        {
+            if (number < 1)
+            {
+                return false;
+            }
+
+            if (number == 1 && number == 2)
+            {
+                return true;
+            }
+
+            for (int i = 2; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
